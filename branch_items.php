8a37a1a059b2e63e58089d05e8f59d8a54c07364
Andrew Nisbet <anisbet@epl.ca>
2015-03-12 16:47:59 -0600

    Added Bootstrap code to branch_items.php.

--- branch_items.php
+++ branch_items.php
@@ -1,66 +1,38 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>AV Incomplete</title>
-<?php 
-	ini_set('error_reporting', E_ALL);
-	// phpinfo();
-?>
-<script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
-<style>
-body {
-  font: 13px/1.3 'Lucida Grande',sans-serif;
-  color: #666;
-}
-.table {
-    display:table;
-}
-.header {
-    display:table-header-group;
-    font-weight:bold;
-	background-color:white;
-}
-.rowGroup {
-    display:table-row-group;
-}
-.row {
-    display:table-row;
-	background-color: 'white';
-	color: #336600;
-}
-.odd {
-  background-color: #E0FFD6;
-  color: #336600;
-}
-
-.cell {
-    display:table-cell;
-    width:10%;
-}
-
-.fixed {
-    position: fixed;
-    top:0; left:0;
-    width: 100%;
-}
-
-</style>
+  <title><?php if (! empty($_GET['branch'])){ echo $_GET['branch']; } ?> AV Incomplete</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
-<body style='text-align:center'>
+<body>
+<div class="container">
+  <div class="jumbotron">
+	<h1>AV Incomplete</h1>
+	<p>''A fuller expression of Self comes from the journey for greater wholeness.'' - Kathryn V. White</p>
+  </div>
+  <p>Incomplete items at <?php if (empty($_GET['branch'])){ echo '??'; } else { echo $_GET['branch']; } ?></p>
+
+<!-- 
 <form action='index.php'>
 <input type='submit' value='back'>
 </form>
-<div id='original' class='table'>
-  <div class='header sticky'>
-    <div class='cell'>Item<br/>Barcode&nbsp;<a id='item_id' href='#' title='sort'><img src='images/sort.gif' /></a></div>
-    <div class='cell'><br/>Title&nbsp;<a id='title' href='#' title='sort'><img src='images/sort.gif' /></a></div>
-    <div class='cell'>Transit<br/>Location</div>
-    <div class='cell'>Date<br/>Reported&nbsp;<a id='date' href='#' title='sort'><img src='images/sort.gif' /></a></div>
-    <div class='cell'>Mark<br/>Discard</div>
-    <div class='cell'>Mark<br/>Complete</div>
-    <div class='cell'>Contacted<br/>Customer</div>
-    <div class='cell'><br/>Comments</div>
-    <div class='cell'>Contact<br/>Info</div>
-  </div>
+-->
+	<div id='original' class='table'>
+		<div class='header sticky row'>
+			<div class='col-sm-2 cell list-group-item active'>Item Barcode&nbsp;<a id='item_id' href='#' title='sort'><img src='images/sort.gif' /></a></div>
+			<div class='col-sm-2 cell list-group-item active'>Title&nbsp;<a id='title' href='#' title='sort'><img src='images/sort.gif' /></a></div>
+			<div class='col-sm-1 cell list-group-item active'>Location</div>
+			<div class='col-sm-2 cell list-group-item active'>Date<a id='date' href='#' title='sort'><img src='images/sort.gif' /></a></div>
+			<div class='col-sm-1 cell list-group-item active'>Discard</div>
+			<div class='col-sm-1 cell list-group-item active'>Complete</div>
+			<div class='col-sm-1 cell list-group-item active'>Contact</div>
+			<div class='col-sm-1 cell list-group-item active'>Comments</div>
+			<div class='col-sm-1 cell list-group-item active'>Info</div>
+		</div>
 <?php 
 ini_set('error_reporting', E_ALL);
 // phpinfo();
@@ -111,26 +83,24 @@ while ($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
 	$dateCreated   = $row['CreateDate'];
 	// replace the spaces because they break the sorting.
 	$title         = preg_replace("/\s+/", "_", $row['Title']);
-	echo "  <div class='rowGroup' date_create='$dateCreated' item_id='$itemId' title='$title'>";
-	echo "    <div class='row'>";
-	echo "      <div class='cell'>" . $row['ItemId'] . "</div>";
-	echo "      <div class='cell'>" . $row['Title'] . "</div>";
-	echo "      <div class='cell'>" . $row['Location'] . "</div>";
-	echo "      <div class='cell'>" . convertANSIDate($row['CreateDate']) . "</div>";
+	echo "  <div class='row rowGroup' date_create='$dateCreated' item_id='$itemId' title='$title'>";
+	echo "      <div class='col-sm-2 cell list-group-item'>" . $row['ItemId'] . "</div>";
+	echo "      <div class='col-sm-2 cell list-group-item'>" . $row['Title'] . "</div>";
+	echo "      <div class='col-sm-1 cell list-group-item'>" . $row['Location'] . "</div>";
+	echo "      <div class='col-sm-2 cell list-group-item'>" . convertANSIDate($row['CreateDate']) . "</div>";
 // TODO Here we have to put in checkboxes, or graphics that we can link to other actions with ajax.
-	echo "      <div class='cell'>data</div>";
-	echo "      <div class='cell'>data</div>";
-	echo "      <div class='cell'>data</div>";
-	echo "      <div class='cell'>data</div>";
-	echo "      <div class='cell'>" . getContact() . "</div>"; // Form for the remove operation.
-	echo "    </div>";
+	echo "      <div class='col-sm-1 cell list-group-item'>data</div>";
+	echo "      <div class='col-sm-1 cell list-group-item'>data</div>";
+	echo "      <div class='col-sm-1 cell list-group-item'>data</div>";
+	echo "      <div class='col-sm-1 cell list-group-item'>data</div>";
+	echo "      <div class='col-sm-1 cell list-group-item'>" . getContact() . "</div>"; // Form for the remove operation.
 	echo "  </div>";
 	$ran++;
 }
 $db->close();
 if ($ran == 0){
 	echo "  <div class='rowGroup'>";
-	echo "    <div class='row'>";
+	echo "    <div class='col-sm-2 cell list-group-item'>";
 	echo "      <div class='cell'>Move along, nothing to see here.</div>";
 	echo "    </div>";
 	echo "  </div>";
@@ -141,11 +111,14 @@ if ($ran == 0){
 // }
 // $db->close();
 ?>
+	</div>
+	<div id='results' class='table'></div>
 </div>
-<div id='results' class='table'></div>
-<form action='index.php'>
+
+<!-- <form action='index.php'>
 <input type='submit' value='back'>
 </form>
+-->
 <script>
 var stickyOffset = $('.sticky').offset().top;
 
@@ -167,11 +140,11 @@ $('a#title').click(
 	function(event) 
 	{
 		event.preventDefault();
-		myArray = $(".header");
-		myArray.push($(".rowGroup"));
+		myArray = $(".rowGroup");
 		isDescending = ! isDescending; 
 		sortIt(myArray, 'title');
-		$("#results").append(myArray);
+		$("div#results").append($(".header"));
+		$("div#results").append(myArray);
 		zebraRows();
 	}
 );
@@ -179,11 +152,11 @@ $('a#date_create').click(
 	function(event) 
 	{
 		event.preventDefault();
-		myArray = $(".header");
-		myArray.push($(".rowGroup"));
+		myArray = $(".rowGroup");
 		isDescending = ! isDescending; 
 		sortIt(myArray, 'date_create');
-		$("#results").append(myArray);
+		$("div#results").append($(".header"));
+		$("div#results").append(myArray);
 		zebraRows();
 	}
 );
@@ -191,11 +164,11 @@ $('a#item_id').click(
 	function(event) 
 	{
 		event.preventDefault();
-		myArray = $(".header");
-		myArray.push($(".rowGroup"));
+		myArray = $(".rowGroup");
 		isDescending = ! isDescending; 
 		sortIt(myArray, 'item_id');
-		$("#results").append(myArray);
+		$("div#results").append($(".header"));
+		$("div#results").append(myArray);
 		zebraRows();
 	}
 );
