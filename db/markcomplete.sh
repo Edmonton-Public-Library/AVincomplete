250496192a88266ff03dcbc243f4b2bc9aea4618
Andrew Nisbet <anisbet@epl.ca>
2016-02-09 13:21:13 -0700

    Update EPL- to EPL_, had to add 'backoff' check to make sure we don't have too many SSH processes running.

--- db/markcomplete.sh
+++ db/markcomplete.sh
@@ -29,6 +29,13 @@
 ####################################################
 HOME=/home/ilsdev/projects/avincomplete/db
 echo `date` >> $HOME/load.log
+test=`ps x | grep avincomplete | wc -l`
+k=1
+if [ $(echo " $test > $k" | bc) -eq 1 ]
+then
+	echo "== process busy." >> $HOME/load.log
+	exit 2
+fi
 if [ -s $HOME/avincomplete.pl ]
 then
 	cd $HOME
