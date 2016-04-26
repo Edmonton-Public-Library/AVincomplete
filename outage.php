c356a7b1a89c30ccce5dbb46c13a59d246c1945d
Andrew Nisbet <anisbet@epl.ca>
2016-04-26 15:07:01 -0600

    Added css and js libs to repo.

---
+++ outage.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>AV Incomplete help</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href='css/style.css'>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
+  <script src="js/jquery-2.1.1.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="index.php">EPL AV Incomplete</a>
+	</div>
+	<div id="navbar" class="navbar-collapse collapse">
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="https://staffweb.epl.ca/"><img src="https://avatars2.githubusercontent.com/u/4293855?v=3&s=20"/></a></li>
+		<li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+		<!-- <li><a href="branch_items.php?branch=ALL"><span class="glyphicon glyphicon-wrench"></span></a></li> -->
+		<li><a href="help.php"><span class="glyphicon glyphicon-info-sign"></span></a></li>
+	  </ul>
+	  <form class="navbar-form navbar-right">
+		<input type="text" class="form-control" placeholder="Search...">
+	  </form>
+	</div>
+  </div>
+</nav>
+<div class="container-fluid">
+	<div class="col-sm-7">
+		<h1 class="sub-header">Aw, nuts!</h1>
+		<p>
+AV incomplete is not available because of on-going network issues, but rest assured we are working hard to get it back on line. In the meantime see your manager for more inforamtion about how to handle AVI materials until we can return to regular service. If you want more information please check Staffweb
+or contact ILS admins at 780-496-4058 for more details.<br/>
+<img src='images/Network.jpg' />
+		</p>
+	</div>
+</div> <!-- container -->
+</body></html>
