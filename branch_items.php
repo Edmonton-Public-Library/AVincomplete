b8cc77fbf6b72c721a39a05368f17085970307fa
Andrew Nisbet <anisbet@epl.ca>
2016-05-11 16:53:02 -0600

    Added padding to body tag on pages with tables so top row doesn't get cut off with nav-bar.

--- branch_items.php
+++ branch_items.php
@@ -16,6 +16,9 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <style>
+  body { padding-top: 70px; }
+  </style>
 </head>
 <body>
 <nav class="navbar navbar-inverse navbar-fixed-top">
